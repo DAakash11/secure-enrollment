a4c7bfa295c56f93dd8214c58f88ce7a18cb3a7c
DAakash11 <akashdhakal011@gmail.com>
2026-07-27 15:31:39 +0100

    Spring Boot App with /hello endpoint

---
+++ src/main/java/com/aakash/qsec/QsecApplication.java
@@ -0,0 +1,13 @@
+package com.aakash.qsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QsecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QsecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aakash/qsec/QsecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aakash.qsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QsecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
